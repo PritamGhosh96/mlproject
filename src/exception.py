15bba4a810ab641d7e53b4d48ab1ff7c365b09ee
Pritam Ghosh <pritamghosh779@gmail.com>
2024-08-30 17:34:32 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,23 @@
+import sys
+import logging
+
+def error_message_detail(error, error_details:sys):
+    _,_,exc_tb = error_details.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occured in python script: \n \
+    Name = [{0}] \n \
+    Line Number = [{1}] \n \
+    Error Message = [{2}]".format(
+            file_name,exc_tb.tb_lineno, str(error)
+    )
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_details:sys) -> None:
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error= error_message, error_details= error_details)
+
+    def __str__(self) -> str:
+        return self.error_message
